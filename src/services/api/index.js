3d65e345ac67e9713bef23a5f87d93011ca324be
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-21 02:08:05 -0600

    add service to api

---
+++ src/services/api/index.js
@@ -0,0 +1,16 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+
+const endPoints = {
+  auth: {
+    login: `${API}/auth/login`,
+    profile: `${API}/profile/my-orders`,
+  },
+  products: {
+    getProducts: `${API}/products`,
+    getSingleProduct: (id) => `${API}/products/${id}`,
+    createProduct: `${API}/products`,
+    editProduct: (id) => `${API}/products/${id}`,
+    deleteProduct: (id) => `${API}/products/${id}`,
+
+  }
+};

08f6c5202c26faf83c9e378403f7866d996fb3c9
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-23 19:39:01 -0600

    add categories page and connect to api

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -6,13 +6,19 @@ const endPoints = {
     profile: `${API}/profile/my-orders`,
   },
   products: {
-    getProducts:(limit, offset) => `${API}/products?limit=${limit}&offset=${offset}`,
+    getProducts: (limit, offset) => `${API}/products?limit=${limit}&offset=${offset}`,
     getSingleProduct: (id) => `${API}/products/${id}`,
     createProduct: `${API}/products`,
     editProduct: (id) => `${API}/products/${id}`,
     deleteProduct: (id) => `${API}/products/${id}`,
-
-  }
+  },
+  categories: {
+    getCategories: `${API}/categories`,
+    getSingleCategory: (id) => `${API}/categories/${id}`,
+    createCategory: `${API}/categories`,
+    editCategory: (id) => `${API}/categories/${id}`,
+    deleteCategory: (id) => `${API}/categories/${id}`,
+  },
 };
 
 export default endPoints;

4495befd81a6b6103866b8cc2b4c3c7bfbbeb047
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-25 22:57:09 -0600

    add categories dashboard and consume the api rest

--- src/pages/dashboard/categories.jsx
+++ src/pages/dashboard/categories.jsx
@@ -7,8 +7,8 @@ import DashboardHeader from '../../components/DashboardHeader';
 
 export default function Dashboard() {
   const [open, setOpen] = useState(false);
-  const products = useFetch(endPoints.products.getProducts(5, 0));
-  console.log(products);
+  const categories = useFetch(endPoints.categories.getCategories);
+  console.log(categories);
   return (
     <>
     <DashboardHeader />
@@ -32,16 +32,7 @@ export default function Dashboard() {
                 <thead className="bg-gray-50">
                   <tr>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Name
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Brand
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Price
-                    </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Stock
+                      Categories in the e-commerce
                     </th>
                     <th scope="col" className="relative px-6 py-3">
                       <span className="sr-only">Edit</span>
@@ -52,26 +43,19 @@ export default function Dashboard() {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {products?.map((product) => (
-                    <tr key={`Product-item-${product.id}`}>
+                  {categories?.map((category) => (
+                    <tr key={`category-item-${category.id}`}>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
-                            <img className="h-10 w-10 rounded-full" src={product.image} alt="image" />
+                            <img className="h-10 w-10 rounded-full" src={category.image} alt="image" />
                           </div>
                           <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{product.name}</div>
-                            <div className="text-sm text-gray-500">{product.description}</div>
+                            <div className="text-sm font-medium text-gray-900">{category.name}</div>
+                            <div className="text-sm text-gray-500">{category.description}</div>
                           </div>
                         </div>
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{product.brand.name}</div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">${product.price}</span>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.stock}</td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <a href="#" className="text-indigo-600 hover:text-indigo-900">
                           Edit

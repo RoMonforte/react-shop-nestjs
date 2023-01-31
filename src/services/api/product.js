644db17175774b90b473185b0d00fee45ea889b4
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-30 20:43:05 -0600

    get token from cookie and send it into header

--- src/services/api/product.js
+++ src/services/api/product.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import endPoints from './index';
+import Cookie from 'js-cookie';
+
+const jwt = Cookie.get('token');
 
 const addProduct = async (body) => {
   const config = {
     headers: {
       accept: '*/*',
       'Content-Type': 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJzdWIiOjMsImlhdCI6MTY3NTEzMTE4NiwiZXhwIjoxNjc1OTk1MTg2fQ.vj4pLDmnUo0Cda-84cTBlMYMMC09bElzZaLHM7j7UDk',
+      Authorization: `Bearer ${jwt}`,
     },
   };
   const response = await axios.post(endPoints.products.createProduct, body, config);
